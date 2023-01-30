7e5b1964ac7243982d40e4868a1c5f347da0223a
Aliyu Nura Salisu <engrsnaliyu@gmail.com>
2023-01-30 21:16:22 +0100

    second commit

--- Device_Example/Device_Example.ino
+++ Device_Example/Device_Example.ino
@@ -1,17 +1,18 @@
 /*A device example for an ultrasonic sensor library*/
 
 #include <ultrasonic.h>
+/*Connect the trigger and echo pin of the ultrasonic sensor to the pin 11 and 12 of the Arduino Uno board for default usage,
+pass the pins numbers to the class constructor*/
+/*Uncomment the following to declare the pins manually*/
 
-int trigpin = 11; //Trigger pin of the ultrasonic sensor that gives the duration data output
-int echopin = 12;  //Echo pin of the ultrasonic sensor that receives the data inout
+//int trigpin = 2 ; //Trigger pin of the ultrasonic sensor that gives the duration data output assuming pin 2
+//int echopin = 3 ;  //Echo pin of the ultrasonic sensor that receives the data input assuming pin 3
+//USS ultasonic (trigpin,echopin);
 
-USS ultrasonic (trigpin,echopin);    //Creating an object of the class USS
+USS ultrasonic;     //For default usage
 
 void setup() {
-  pinMode(echopin, INPUT);
-  pinMode(trigpin, OUTPUT);
-  Serial.begin(9600);
-
+ ultrasonic.Setup();
 }
 
 void loop() {
